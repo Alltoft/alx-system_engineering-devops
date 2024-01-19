f97002636364712212f8d0fecaadecb5b20f3871
Emran <ouakouriimran@gmail.com>
2024-01-19 18:40:05 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+#a puppet that create a file in /tmp.
+
 file { '/tmp/school':
   ensure  => 'present',
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
